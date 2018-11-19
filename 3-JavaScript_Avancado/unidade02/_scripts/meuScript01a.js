f29fc7214391d2a932cb4a766f1ba407bc83dfb9
Felipe Murakami <fhmurakami@gmail.com>
2018-11-19 11:10:34 -0200

    Add JavaScript - DOM - Como acessar elementos com getElementById()

---
+++ 3-JavaScript_Avancado/unidade02/_scripts/meuScript01a.js
@@ -0,0 +1,7 @@
+var elemento = document.getElementById("opcoesPizza")
+
+// console.log(elemento.children)
+// console.log(elemento.children[0].innerHTML)
+// console.log(elemento.childElementCount)
+// console.log(elemento.firstElementChild.textContent)
+console.log(elemento.firstChild)
