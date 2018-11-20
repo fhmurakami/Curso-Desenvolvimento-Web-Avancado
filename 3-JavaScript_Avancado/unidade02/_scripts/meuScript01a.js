1fbd9978afcccb1edc4e5d1a2aa6f5100a229525
Felipe Murakami <fhmurakami@gmail.com>
2018-11-19 22:10:00 -0200

    Add JavaScript - DOM - Usar um laço para mostrar todos os elementos filhos

--- 3-JavaScript_Avancado/unidade02/_scripts/meuScript01a.js
+++ 3-JavaScript_Avancado/unidade02/_scripts/meuScript01a.js
@@ -4,4 +4,9 @@ var elemento = document.getElementById("opcoesPizza")
 // console.log(elemento.children[0].innerHTML)
 // console.log(elemento.childElementCount)
 // console.log(elemento.firstElementChild.textContent)
-console.log(elemento.firstChild)
+// console.log(elemento.firstChild)
+
+var tamanho = elemento.childElementCount
+for (var i = 0; i < tamanho; i++) {
+    console.log(elemento.children[i].innerHTML)
+}
