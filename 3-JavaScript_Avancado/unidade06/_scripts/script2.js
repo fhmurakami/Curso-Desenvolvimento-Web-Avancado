3c3735a81fc0ad1cd8d8e07255e05f96e12274ff
Felipe Murakami <fhmurakami@gmail.com>
2018-11-20 16:59:04 -0200

    Add JavaScript - Eventos diversos - Mostrar janela para capturar Lead

--- 3-JavaScript_Avancado/unidade06/_scripts/script2.js
+++ 3-JavaScript_Avancado/unidade06/_scripts/script2.js
@@ -3,6 +3,11 @@ function pegarPosicaoMouse(e) {
     var posY = document.querySelector("#posicaoY")
     posX.innerHTML = e.clientX
     posY.innerHTML = e.clientY
+
+    if (e.clientY < 10) {
+        var minhaJanela = document.querySelector("#janela")
+        minhaJanela.style.display = "block"
+    }
 }
 
 document.addEventListener("mousemove", pegarPosicaoMouse)
