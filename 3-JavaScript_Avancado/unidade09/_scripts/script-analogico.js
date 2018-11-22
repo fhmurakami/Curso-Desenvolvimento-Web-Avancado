7aec55481c4a35514cddda84dc1310aa24e837d8
Felipe Murakami <fhmurakami@gmail.com>
2018-11-22 12:37:08 -0200

    Add JavaScript - Relógio Digital e Analógico - Aprimorar o ponteiro do segundo

--- 3-JavaScript_Avancado/unidade09/_scripts/script-analogico.js
+++ 3-JavaScript_Avancado/unidade09/_scripts/script-analogico.js
@@ -5,14 +5,15 @@ function mostrarHora() {
     var hr = agora.getHours()
     var min = agora.getMinutes()
     var seg = agora.getSeconds()
+    var mili = agora.getMilliseconds()
 
     var ponteiroHora = hr * 30 + min / 2
-    var ponteiroMin = min * 6 + seg / 10
-    var ponteiroSeg = seg * 6
+    var ponteiroMin = min * 6 + seg * 6 / 60
+    var ponteiroSeg = seg * 6 + mili * 6 / 1000
 
     hora.style.transform = `rotate(${ponteiroHora}deg)`
     minuto.style.transform = `rotate(${ponteiroMin}deg)`
     segundo.style.transform = `rotate(${ponteiroSeg}deg)`
 }
 
-var intervalo = setInterval(mostrarHora, 1000)
+var intervalo = setInterval(mostrarHora, 1)
